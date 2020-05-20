1768b205e39d99ecf9c49a8738c0fd7982cec1b3
Patrick Sime <psime86@gmail.com>
2020-05-20 15:39:28 -0500

    updated models

--- models/book.js
+++ models/book.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  _id: { type: String },
   title: { type: String },
   authors: { type: Array },
   description: { type: String },
